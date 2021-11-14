9828b11ec4cc3736265944549c82195d8fc795c7
chisa-f <kiki4537fer2cat@gmail.com>
2021-11-13 23:14:15 -0500

    Add Chat Functionality

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,8 +1,28 @@
-const chatArea = document.querySelector(".chat-area");
-
+var messages = []; //list of messages
 //Action: when clicking the "Send" button
 function sendMessage() {
+  var chatArea = document.querySelector(".message");
+  if (chatArea.style.display === "none") {
+    chatArea.style.display = "block";
+  }
   var message = document.getElementById("textbox").value;
-  chatArea.classList.remove(".hidden");
-  alert("The message you sent is " + message);
+  messages.push(message); //storing the messages in the list
+  document.querySelector(".chat-text").textContent = message;
+  for (index = 0; index < messages.length; index++) {
+    console.log(messages[index]);
+  }
+  // messages
+  //   // .map((item) => createMessage(item))
+  //   .forEach((el) => document.getElementById("chat-area").appendChild(el));
+  document
+    .getElementById("chat-section")
+    .appendChild(createMessage(messages[messages.length - 1]));
 }
+
+var text = document.getElementById("textbox").value;
+const createMessage = (text) => {
+  const el = document.createElement("div"); //add something like this.
+  el.innerHTML = text;
+  el.className = "chat-area me shadow-lg";
+  return el;
+};
